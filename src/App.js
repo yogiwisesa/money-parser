b533f0fcf235c64585f463653af427c64106901e
yogiwisesa <yogiwisesa@gmail.com>
2019-04-25 22:59:48 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,149 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  state = {
+    textInput: '',
+    isInputValid: true,
+    denominations: ''
+  };
+
+  /**
+   * Set textInput state with new event value
+   * from input text when input text changed
+   * and run validation for it.
+   *
+   * @param {event} e - Event from input text
+   */
+  handleTextInputChanged = e => {
+    const textInput = e.target.value;
+    this.handleValidation(textInput);
+
+    this.setState({
+      textInput,
+      denominations: ''
+    });
+  };
+
+  /**
+   * Handle submission called when button calculate get clicked or Enter key pressed
+   * Set state with converted denominations
+   *
+   * @param {event} e - Event from key down or button clicked
+   */
+  handleSubmit = e => {
+    if (e.key === 'Enter' || e.key === undefined) {
+      if (this.state.isInputValid) {
+        const parsed = this.handleParse(this.state.textInput);
+        const denominations = `= ${this.handleConvertToFraction(parsed)}.`;
+        this.setState({
+          denominations
+        });
+      }
+    }
+  };
+
+  /**
+   * Validate the value of the input with regex
+   *
+   * @param {string} text - Value of the input text
+   */
+  handleValidation(text) {
+    // Test input string using Regex
+    const pattern = /^(Rp|Rp |\d[0]+|)(\d{1,3}(\.\d{3}){1,}|\d+)(\,(\d[0])){0,1}$/;
+    let isInputValid = pattern.test(text);
+
+    if (text === '') {
+      isInputValid = true;
+    }
+
+    this.setState({
+      isInputValid
+    });
+  }
+
+  /**
+   * Renove unnecessary characters from input text
+   *
+   * @param {string} text - Value of the input text
+   */
+  handleParse(text) {
+    return text
+      .replace(/,00/g, '')
+      .replace(/ /g, '')
+      .replace(/Rp/g, '')
+      .replace(/\./g, '');
+  }
+
+  /**
+   * Convert cleaned input text value into string that contains
+   * rupiah fraction and the number needed.
+   * Available fractions: 100000, 50000, 20000, 10000, 5000, 1000, 500, 100, 50.
+   *
+   * @param {string} text - Cleaned input text
+   */
+  handleConvertToFraction(text) {
+    // Convert to integer
+    let rupiah = parseInt(text, 10);
+
+    // List available rupiah fraction
+    const fractions = [100000, 50000, 20000, 10000, 5000, 1000, 500, 100, 50];
+
+    // Converted contains fraction and the number of the fraction that needed
+    let converted = '';
+    let left = fractions.reduce((left, fraction) => {
+      const div = Math.floor(left / fraction);
+      if (div > 0) {
+        if (converted !== '') {
+          converted += ', ';
+        }
+        converted += `${div}x Rp${fraction}`;
+
+        return (left %= fraction);
+      }
+      return left;
+    }, rupiah);
+
+    // If there is no more smaller fraction but we still have rupiah
+    if (left > 0) {
+      if (converted !== '') {
+        converted += ', ';
+      }
+      converted += `left Rp${left} (no available fraction)`;
+    }
+
+    return converted;
+  }
+
+  render() {
+    const { isInputValid, denominations, textInput } = this.state;
+    return (
+      <div className="App">
+        <div className="Card">
+          <h1>Money Parser</h1>
+          <line />
+          <input
+            type="text"
+            className={!isInputValid ? 'input--failed' : ''}
+            placeholder="Your rupiah value here..."
+            onChange={this.handleTextInputChanged}
+            onKeyDown={this.handleSubmit}
+            value={textInput}
+          />
+          {!isInputValid && <p className="invalid-input-label">Your input isn't valid!</p>}
+          <p>
+            {' '}
+            Denominations for {textInput ? textInput : '-'} <b>{denominations}</b>
+          </p>
+          <button onClick={this.handleSubmit}>Calculate</button>
+          <i>
+            After finished your input, you can press the 'Calculate' button or
+            press enter.
+          </i>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,79 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let wrapper;
+beforeEach(() => {
+  wrapper = shallow(<App />);
 });
+
+it('Renders all components correctly', () => {
+  expect(wrapper.find('input').length).toEqual(1);
+  expect(wrapper.find('p').length).toEqual(1);
+  expect(wrapper.find('button').length).toEqual(1);
+});
+
+/**
+ * Function to test local state and 
+ * rendered denomination based on input text
+ * 
+ * @param {string} input - Value for input text
+ * @param {boolean} inputValid - Expected value for inputValid state 
+ * @param {string} denominations - Expected denomination
+ * @param {string} key  - Value for trigger calculate button click or enter pressed
+ */
+const testCalculate = (input, inputValid, denominations, key) => {
+  it(`Should calculate & render denominations correctly when input text: '${input}'`, () => {
+    const event = {
+      target: {
+        value: input
+      }
+    };
+
+    const mockEventClick = {
+      key: undefined
+    };
+
+    const mockEventEnter = {
+      key: 'Enter'
+    };
+
+    const inputText = wrapper.find('input');
+    inputText.simulate('change', event);
+
+    expect(wrapper.state('textInput')).toEqual(input);
+
+    if (key === 'enter') {
+      inputText.simulate('keyDown', mockEventEnter);
+    } else {
+      const button = wrapper.find('button');
+      button.simulate('click', mockEventClick);
+    }
+
+    expect(wrapper.state('isInputValid')).toEqual(inputValid);
+    expect(wrapper.state('denominations')).toContain(denominations);
+
+    expect(wrapper.find('b').text()).toContain(denominations);
+  });
+};
+
+/**
+ * In Example test case there is '2000' fraction but in the requirement there isn't.
+ */
+
+testCalculate('15000', true, '1x Rp10000, 1x Rp5000', 'enter');
+testCalculate('Rp3900', true, '3x Rp1000, 1x Rp500, 4x Rp100', 'click');
+testCalculate('12510', true, '1x Rp10000, 2x Rp1000, 1x Rp500, left Rp10 (no available fraction)', 'enter');
+
+testCalculate('18.215', true, '1x Rp10000, 1x Rp5000, 3x Rp1000, 2x Rp100, left Rp15 (no available fraction)', 'enter');
+testCalculate('Rp17500', true, '1x Rp10000, 1x Rp5000, 2x Rp1000, 1x Rp500', 'enter');
+testCalculate('Rp17.500,00', true, '1x Rp10000, 1x Rp5000, 2x Rp1000, 1x Rp500', 'enter');
+testCalculate('Rp 120.325', true, '1x Rp100000, 1x Rp20000, 3x Rp100, left Rp25 (no available fraction)', 'enter');
+testCalculate('005.000', true, '1x Rp5000', 'enter');
+testCalculate('001000', true, '1x Rp1000', 'enter');
+
+
+testCalculate('17,500', false, '', 'enter');
+testCalculate('2 500', false, '', 'click');
+testCalculate('3000 Rp', false, '', 'enter');
+testCalculate('Rp', false, '', 'click');
